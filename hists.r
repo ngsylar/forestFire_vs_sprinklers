b2f02be7903ef205fa042e408c5e2c834bba50f1
ngsylar <ngsylar@gmail.com>
2022-03-30 15:40:17 -0300

    hists

--- hists.r
+++ hists.r
@@ -1,19 +1,34 @@
-# hist(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.0], breaks=100, main="Histogram for Average Cluster Size", xlab="Average Cluster Size")
-
-# hist(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.2], breaks=100, main="Histogram for Average Cluster Size", xlab="Average Cluster Size")
-
-# hist(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.4], breaks=100, main="Histogram for Average Cluster Size", xlab="Average Cluster Size")
-
-# hist(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.6], breaks=100, main="Histogram for Average Cluster Size", xlab="Average Cluster Size")
-
-# hist(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.8], breaks=100, main="Histogram for Average Cluster Size", xlab="Average Cluster Size")
+# ------------------------------------------------------
+# sprinkler density == 0.0
 
 hist(model_data_2022_03_24_19_02_50_530124$AverageClusterSize[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.0], breaks=100, main="Histogram for Average Cluster Size with 0.0 of Sprinkler Density", xlab="Average Cluster Size")
 
+mean(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.0], breaks=100)
+
+# ------------------------------------------------------
+# sprinkler density == 0.2
+
 hist(model_data_2022_03_24_19_02_50_530124$AverageClusterSize[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.2], breaks=100, main="Histogram for Average Cluster Size with 0.2 of Sprinkler Density", xlab="Average Cluster Size")
+
+mean(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.2], breaks=100)
+
+# ------------------------------------------------------
+# sprinkler density == 0.4
 
 hist(model_data_2022_03_24_19_02_50_530124$AverageClusterSize[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.4], breaks=100, main="Histogram for Average Cluster Size with 0.4 of Sprinkler Density", xlab="Average Cluster Size")
 
+mean(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.4], breaks=100)
+
+# ------------------------------------------------------
+# sprinkler density == 0.6
+
 hist(model_data_2022_03_24_19_02_50_530124$AverageClusterSize[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.6], breaks=100, main="Histogram for Average Cluster Size with 0.6 of Sprinkler Density", xlab="Average Cluster Size")
 
+mean(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.6], breaks=100)
+
+# ------------------------------------------------------
+# sprinkler density == 0.8
+
 hist(model_data_2022_03_24_19_02_50_530124$AverageClusterSize[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.8], breaks=100, main="Histogram for Average Cluster Size with 0.8 of Sprinkler Density", xlab="Average Cluster Size")
+
+mean(model_data_2022_03_24_19_02_50_530124$Clusters[model_data_2022_03_24_19_02_50_530124$sprinklerDensity==0.8], breaks=100)
